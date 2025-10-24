5b425ce77c51948e7bb3dcef0be9e3ea357c9396
Joe Keisker <jkeisker@protonmail.com>
2025-10-24 14:31:45 -0500

    changes for chapters 26 & 27

---
+++ SimpleShoppingList/Controllers/ItemController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleShoppingList.Controllers
+{
+    public class ItemController : ApiController
+    {
+        // POST: api/Item
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Item/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Item/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
